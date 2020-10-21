c30a8960acba98d0f7b002cec1921b3c373f5dc1
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-10-21 12:20:49 +0900

    パスワード設定

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/todo', 'TodoController@index');
+Auth::routes();
+
+Route::group(['middleware' => ['web', 'auth']], function () {
+    Route::resource('todo', 'TodoController', ['only' => [
+        'index', 'show'
+    ]]);
+});
