4b2e8bf6a340b19f8ecd54ebb6f5bd7ba0120acf
yukaaaaaaa <19s019yuka@gmail.com>
2020-12-09 12:29:41 +0900

    font size変更

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="col-md-10">
-            <h2 class="text-muted py-3 float-left">やること一覧</h2>
+            <h2 class="text-muted py-3 float-left"style="font-size:40px">やること一覧</h2>
             <div class="float-right my-3">
                 <a href="/todo/create/" class="btn btn-primary"><i class="fas fa-plus mr-2"></i>新規作成</a>
             </div>
